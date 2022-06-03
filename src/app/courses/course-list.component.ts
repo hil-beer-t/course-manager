c4378356bf0e3c9ca7995f2dc83b5a4680160ad1
Hilbert <hil.beer.t.git@gmail.com>
2022-06-03 03:08:26 -0300

    course list

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent {
+
+  courses: Course[] = [
+    {
+      id: 1,
+      name: 'Angular: Forms',
+      imageUrl: '',
+      price: 99.99,
+      code: 'XPS-8796',
+      duration: 120,
+      rating: 4.2,
+      releaseDate: 'December, 2, 2019'
+    },
+    {
+      id: 2,
+      name: 'Angular: http',
+      imageUrl: '',
+      price: 50.00,
+      code: 'LKL-1296',
+      duration: 80,
+      rating: 5,
+      releaseDate: 'December, 10, 2019'
+    }
+  ]
+
+}
